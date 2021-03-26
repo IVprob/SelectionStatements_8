89a9d78affdcdbecb987e01d07c018914fc78926
IVProb <jnoojinjr@gmail.com>
2021-03-26 10:07:59 -0500

    initial commit

--- SelectionStatements_8/Program.cs
+++ SelectionStatements_8/Program.cs
@@ -6,24 +6,7 @@ namespace SelectionStatements_8
     {
         static void Main(string[] args)
         {
-                var r = new Random();
-                var favNumber = r.Next(1, 1000);
-
-                Console.WriteLine("Guess a number between 1 and 1000.");
-                var userInput = int.Parse(Console.ReadLine());
-    
-                if (favNumber > userInput)
-                {
-                    Console.WriteLine($"Your guess of {userInput} is too low. {favNumber} is the answer.");
-                }
-                else if (favNumber < userInput)
-                {
-                    Console.WriteLine($"Your guess of {userInput} is to high. {favNumber} is the answer.");
-                }
-                else
-                {
-                    Console.WriteLine($"You nailed it!");
-                }
+                
         }
     }
 }
